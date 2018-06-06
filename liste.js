95a376545371d40381b23048393ed01e7ad2aee5
HOUMADI BADRANE <badrane976@gmailcom>
2018-06-06 20:14:05 +0400

    amelioration

--- liste.js
+++ liste.js
@@ -1,21 +1,17 @@
 $(document).ready(function(){
     $("button").click(function(){
-        $.ajax({url:"http://localhost:3007/liste", 
+        $.ajax({url:"http://localhost:3007/liste",
+                methode : 'get',
             success : function(data){
-            var tab = [];
             for (var id in data){
-                tab.push ('<li class="list-group-item">' + data[id].name.first + " " + data[id].name.last + '</li>');
+                $("#l").prepend('<li class="list-group-item">' + data[id].name.first + " " + data[id].name.last + '</li>');
+                // $("#l").append('<li class="list-group-item">' + data[id].name.first + " " + data[id].name.last + '</li>');
+                // $("#l").html('<li class="list-group-item">' + data[id].name.first + " " + data[id].name.last + '</li>');
             }
-            $("#l").html(tab);
+            
         }});
     });
 });
 
-$.ajax({url:"http://localhost:3007/liste", 
-    success : function(data){
-    var tab = [];
-    for (var id in data){
-        tab.push ('<li class="list-group-item">' + data[id].name.first + " " + data[id].name.last + '</li>');
-    }
-    $("#l").html(tab);
-}});
+
+
