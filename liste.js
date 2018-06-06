601a01fb2fd0d159a2931bb399567176965103b5
HOUMADI BADRANE <badrane976@gmailcom>
2018-06-06 20:18:50 +0400

    indentation list.js

--- liste.js
+++ liste.js
@@ -1,15 +1,17 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $.ajax({url:"http://localhost:3007/liste",
-                methode : 'get',
-            success : function(data){
-            for (var id in data){
-                $("#l").prepend('<li class="list-group-item">' + data[id].name.first + " " + data[id].name.last + '</li>');
-                // $("#l").append('<li class="list-group-item">' + data[id].name.first + " " + data[id].name.last + '</li>');
-                // $("#l").html('<li class="list-group-item">' + data[id].name.first + " " + data[id].name.last + '</li>');
+$(document).ready(function () {
+    $("button").click(function () {
+        $.ajax({
+            url: "http://localhost:3007/liste",
+            methode: 'get',
+            success: function (data) {
+                for (var id in data) {
+                    $("#l").prepend('<li class="list-group-item">' + data[id].name.first + " " + data[id].name.last + '</li>');
+                    // $("#l").append('<li class="list-group-item">' + data[id].name.first + " " + data[id].name.last + '</li>');
+                    // $("#l").html('<li class="list-group-item">' + data[id].name.first + " " + data[id].name.last + '</li>');
+                }
+
             }
-            
-        }});
+        });
     });
 });
 
